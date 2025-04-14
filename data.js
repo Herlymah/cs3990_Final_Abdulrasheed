8ffe5f01a1fcb633249d2092e693ea374dcc3f15
Herlymah <5087513@nwpolytech.ca>
2025-04-14 14:29:41 -0600

    Update data.js

--- data.js
+++ data.js
@@ -3,28 +3,28 @@ const hannaShop = [
       group: "Coffee beans",
       name: "Colombian Supremo Coffee",
       price: 21.89,
-      image: "Images/supremoCoffee.png",
+      image: "supremoCoffee.png",
       descr: "The Colombian Supremo Coffee Beans offer a mellow cup with complex aromas and rich flavours."
     },
     {
       group: "Coffee beans",
       name: "Ethiopian Sidamo Coffee",
       price: 23.49,
-      image: "Images/ethopian-coffee.jpg",
+      image: "ethopian-coffee.jpg",
       descr: "Ethiopian Sidamo is consistently rated one of the best coffees in the world. It begins with fruity notes."
     },
     {
       group: "Coffee and Tea Accessories",
       name: "Milk Frother",
       price: 13.97,
-      image: "Images/milk-frother.jpg",
+      image: "milk-frother.jpg",
       descr: "Make Creamy Froth Quickly: If you love your morning coffee filled with froth and foam, the portable milk frother is for you."
     },
     {
       group: "Coffee makers",
       name: "Braun Drip Coffee Maker",
       price: 139,
-      image: "Images/coffee-maker.jpg",
+      image: "coffee-maker.jpg",
       descr: "Braun's PureFlavor technology ensures your coffee is made at the right temperature and brewing time."
     }
   ];
